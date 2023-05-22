37673d75295144ad6b503f1424348db76ac1af1a
Roche <108253548+flzroche@users.noreply.github.com>
2023-05-22 10:07:04 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import "./styles.css";
+
+// export default function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hello CodeSandbox</h1>
+//       <h2>Start editing to see some magic happen!</h2>
+//     </div>
+//   );
+// }
+
+// var num = 44;
+// console.log(num - 43);
+
+// var str = "test文字列";
+// console.log(str);
+
+// // var 変数は上書き可能
+// var str = "上書き";
+// console.log(str);
+
+// let val2 = "let変数";
+// console.log(val2);
+
+// // let 上書き可能
+
+// val2 = "let上書";
+// console.log(val2);
+
+// // ↑letは再宣言不可能
+// // let val2 とするとだめ
+
+const val3 = "const変数";
+console.log(val3);
+// constは厳密な変数であり，上書き不可能
+// const val3 = "上書き";
+// console.log(val3);
+val3 = "普通に文字列上書きかな";
+console.log(val3);
+
+// => 上書きできない
